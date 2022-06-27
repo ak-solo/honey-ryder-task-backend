f795a72bade94663f692c952016cc970f5650b07
ak-solo <ak.solo.2022@gmail.com>
2022-06-27 23:19:33 +0900

    タスクIDを作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/TaskId.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスクID
+    /// </summary>
+    public record TaskId
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TaskId"/> class.
+        /// タスクIDを生成します。
+        /// </summary>
+        /// <param name="value">ULID形式の文字列を指定します。</param>
+        public TaskId(string value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// タスクID
+        /// </summary>
+        public string Value { get; }
+
+        /// <summary>
+        /// タスクIDを新規採番します。
+        /// </summary>
+        /// <returns>
+        /// 新しく採番したタスクIDを返します。
+        /// </returns>
+        public static TaskId NewId()
+        {
+            return new TaskId(value: NUlid.Ulid.NewUlid().ToString());
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTest/Tests/Domain/Models/Tasks/TaskId_Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Shared;
+using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTest.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// TaskId - test
+    /// </summary>
+    public class TaskId_Test
+    {
+        [Fact(DisplayName = "[TaskId(value)] 指定した値でタスクIDを生成できる")]
+        public void TaskId_Test1()
+        {
+            // arrange
+            var value = "01D0KDBRASGD5HRSNDCKA0AH53";
+
+            // act
+            var id = new TaskId(value);
+
+            // assert
+            Assert.Equal(value, id.Value);
+        }
+
+        [Fact(DisplayName = "[NewId()] 新しいタスクID(ULID)を採番できる")]
+        public void NewId_Test1()
+        {
+            // act
+            var newId = TaskId.NewId();
+
+            // assert
+            Assert.True(ULID.IsULID(newId.Value));
+        }
+    }
+}
