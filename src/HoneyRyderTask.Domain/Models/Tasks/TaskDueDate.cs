985ae4633afa9a8c40aad2fb05880275e364079c
ak-solo <ak.solo.2022@gmail.com>
2022-07-03 12:11:34 +0900

    タスク期限を作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/TaskDueDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク期限
+    /// </summary>
+    public class TaskDueDate
+    {
+        /// <summary>
+        /// タスク期限を生成します。
+        /// </summary>
+        /// <param name="value">タスク期限となる値を指定します。</param>
+        public TaskDueDate(DateTime value)
+        {
+            this.Value = value.Date;
+        }
+
+        /// <summary>
+        /// タスク期限
+        /// </summary>
+        public DateTime Value { get; }
+    }
+}
---
+++ src/HoneyRyderTaskTest/Tests/Domain/Models/Tasks/TaskDueDate_Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTest.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// TaskDueDate - test
+    /// </summary>
+    public class TaskDueDate_Test
+    {
+        [Fact(DisplayName = "[TaskDueDate(value)] 指定した値でタスク期限を生成できる")]
+        public void TaskDueDate_Test1()
+        {
+            // arrange
+            var value = new DateTime(2022, 1, 1);
+
+            // act
+            var dueDate = new TaskDueDate(value);
+
+            // assert
+            Assert.Equal(value, dueDate.Value);
+        }
+
+        [Fact(DisplayName = "[TaskDueDate(value)] 指定した値の時間部分は丸めて日付のみでタスク期限を生成する")]
+        public void TaskDueDate_Test2()
+        {
+            // arrange
+            var value = new DateTime(2022, 1, 1, 12, 30, 45);
+
+            // act
+            var dueDate = new TaskDueDate(value);
+
+            // assert
+            Assert.NotEqual(value, dueDate.Value);      // 時間を含めると不一致
+            Assert.Equal(value.Date, dueDate.Value);    // 日付だけでみると一致
+        }
+    }
+}
