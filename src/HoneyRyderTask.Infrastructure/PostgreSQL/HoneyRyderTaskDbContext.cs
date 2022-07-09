c45f737efaf54abbc9f59e7ab495cf169ca8db7b
ak-solo <ak.solo.2022@gmail.com>
2022-07-09 16:01:38 +0900

    DBコンテキスト作成

---
+++ src/HoneyRyderTask.Infrastructure/PostgreSQL/HoneyRyderTaskDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace HoneyRyderTask.Infrastructure.PostgreSQL
+{
+    /// <summary>
+    /// DBコンテキスト
+    /// </summary>
+    public class HoneyRyderTaskDbContext : DbContext
+    {
+        /// <summary>
+        /// DB構成
+        /// </summary>
+        /// <param name="optionsBuilder">オプションビルダー</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<HoneyRyderTaskDbContext>()
+                .Build();
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
--- src/HoneyRyderTask.UseCase/Services/Tasks/RegisterTask/RegisterTaskUseCase.cs
+++ src/HoneyRyderTask.UseCase/Services/Tasks/RegisterTask/RegisterTaskUseCase.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Transactions;
+﻿using System.Transactions;
 using HoneyRyderTask.Domain.Models.Tasks;
 
 namespace HoneyRyderTask.UseCase.Services.Tasks.RegisterTask
