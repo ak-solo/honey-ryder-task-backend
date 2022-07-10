3cdcdc6816f36003cc9ff0f54e6a9ae4455a46b3
ak-solo <ak.solo.2022@gmail.com>
2022-07-10 18:18:45 +0900

    タスクリポジトリ作成

---
+++ src/HoneyRyderTask.Infrastructure/PostgreSQL/Repositories/Tasks/TaskRepository.cs
@@ -0,0 +1,47 @@
+﻿using HoneyRyderTask.Domain.Models.Tasks;
+using HoneyRyderTask.Infrastructure.PostgreSQL.DataModels.Tasks;
+
+namespace HoneyRyderTask.Infrastructure.PostgreSQL.Repositories.Tasks
+{
+    /// <summary>
+    /// タスクリポジトリ
+    /// </summary>
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly HoneyRyderTaskDbContext context;
+
+        /// <summary>
+        /// タスクリポジトリを生成します。
+        /// </summary>
+        /// <param name="context">DBコンテキスト</param>
+        public TaskRepository(HoneyRyderTaskDbContext context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// タスクをリポジトリに追加します。
+        /// </summary>
+        /// <param name="task">タスク</param>
+        public void Add(Domain.Models.Tasks.Task task)
+        {
+            var data = ToDataModel(task);
+            this.context.Add(data);
+            this.context.SaveChanges();
+        }
+
+        private static TaskDataModel ToDataModel(Domain.Models.Tasks.Task task)
+        {
+            return new TaskDataModel()
+            {
+                TaskId = task.Id.Value,
+                Title = task.Title.Value,
+                Description = task.Description.Value,
+                Status = (int)task.Status,
+                DueDate = task.DueDate?.Value,
+                CreationDate = task.CreationDate.Value,
+                CompletionDate = task.CompletionDate?.Value,
+            };
+        }
+    }
+}
