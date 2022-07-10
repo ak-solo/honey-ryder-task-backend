489b8ec4a6757a54dc6ab278cf4bdd15ff3424f1
ak-solo <ak.solo.2022@gmail.com>
2022-07-10 20:14:57 +0900

    Swaggerの説明にXMLコメントを使用

--- src/HoneyRyderTask.WebAPI/Program.cs
+++ src/HoneyRyderTask.WebAPI/Program.cs
@@ -1,4 +1,5 @@
-﻿using HoneyRyderTask.Domain.Models.Shared;
+﻿using System.Reflection;
+using HoneyRyderTask.Domain.Models.Shared;
 using HoneyRyderTask.Domain.Models.Tasks;
 using HoneyRyderTask.Domain.Services.Shared;
 using HoneyRyderTask.Infrastructure.PostgreSQL;
@@ -13,7 +14,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddSingleton<IDateTimeProvider, DefaultDateTimeProvider>();
 
