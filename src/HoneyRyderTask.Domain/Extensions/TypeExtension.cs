cb8489221a37d1824b74a7716b07c89dbc0bc3e2
ak-solo <ak.solo.2022@gmail.com>
2022-07-12 19:06:15 +0900

    Type拡張メソッド作成

---
+++ src/HoneyRyderTask.Domain/Extensions/TypeExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace System
+{
+    /// <summary>
+    /// Type（拡張メソッド）
+    /// </summary>
+    public static class TypeExtension
+    {
+        /// <summary>
+        /// 指定の属性を持っているかどうかを返します。
+        /// 持っている場合はtrue。それ以外はfalseを返します。
+        /// </summary>
+        /// <typeparam name="TAttributeType">属性タイプ</typeparam>
+        /// <param name="type">タイプ</param>
+        /// <returns></returns>
+        public static bool HasAttribute<TAttributeType>(this Type type)
+        {
+            var attr = type.GetCustomAttributes(typeof(TAttributeType), inherit: false);
+            return attr.Count() > 0;
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTest/Tests/Domain/Extensions/TypeExtension_Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using HoneyRyderTask.UseCase.Attributes;
+using Xunit;
+
+namespace HoneyRyderTaskTest.Tests.Domain.Extensions
+{
+    /// <summary>
+    /// TypeExtension -test
+    /// </summary>
+    public class TypeExtension_Test
+    {
+        [Fact(DisplayName = "指定した属性を持ったクラスであれば true を返す。")]
+        public void HasAttribute_Test1()
+        {
+            // act
+            var actual = typeof(Class1).HasAttribute<UseCaseAttribute>();
+
+            // assert
+            Assert.True(actual);
+        }
+
+        [Fact(DisplayName = "指定した属性を持っていないクラスであれば false を返す。")]
+        public void HasAttribute_Test2()
+        {
+            // act
+            var actual = typeof(Class2).HasAttribute<UseCaseAttribute>();
+
+            // assert
+            Assert.False(actual);
+        }
+
+        [UseCase]
+        private class Class1
+        {
+        }
+
+        private class Class2
+        {
+        }
+    }
+}
