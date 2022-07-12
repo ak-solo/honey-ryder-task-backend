8485cd92c11ff0daad5eee456a8806b124901bda
ak-solo <ak.solo.2022@gmail.com>
2022-07-12 20:50:26 +0900

    不足していたXMLコメントを追加

--- src/HoneyRyderTask.Domain/Extensions/TypeExtension.cs
+++ src/HoneyRyderTask.Domain/Extensions/TypeExtension.cs
@@ -9,11 +9,11 @@ namespace System
     {
         /// <summary>
         /// 指定の属性を持っているかどうかを返します。
-        /// 持っている場合はtrue。それ以外はfalseを返します。
+        /// 指定の属性を持っている場合はtrue。それ以外はfalseを返します。
         /// </summary>
         /// <typeparam name="TAttributeType">属性タイプ</typeparam>
         /// <param name="type">タイプ</param>
-        /// <returns></returns>
+        /// <returns>指定の属性を持っている場合はtrue。それ以外はfalseを返します。</returns>
         public static bool HasAttribute<TAttributeType>(this Type type)
         {
             var attr = type.GetCustomAttributes(typeof(TAttributeType), inherit: false);
--- src/HoneyRyderTask.UseCase/Helpers/UseCaseHelper.cs
+++ src/HoneyRyderTask.UseCase/Helpers/UseCaseHelper.cs
@@ -2,12 +2,17 @@
 
 namespace HoneyRyderTask.UseCase.Helpers
 {
+    /// <summary>
+    /// ユースケース - helper
+    /// </summary>
     public static class UseCaseHelper
     {
         /// <summary>
         /// ユースケース層のアセンブリを取得します。
         /// </summary>
-        /// <returns></returns>
+        /// <returns>
+        /// ユースケース層のアセンブリを返します。
+        /// </returns>
         public static Assembly GetAssembly()
         {
             return Assembly.GetExecutingAssembly();
