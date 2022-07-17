bee6296537914442afd6fadff0cac1a103561574
ak-solo <ak.solo.2022@gmail.com>
2022-07-17 13:04:56 +0900

    タイプセーフEnum抽象クラスを作成

---
+++ src/HoneyRyderTask.Domain/Models/Shared/TypeSafeEnum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+
+namespace HoneyRyderTask.Domain.Models.Shared
+{
+    /// <summary>
+    /// タイプセーフEnum
+    /// </summary>
+    public abstract record TypeSafeEnum<T>
+        where T : TypeSafeEnum<T>
+    {
+        /// <summary>
+        /// 指定の値でタイプセーフEnumを生成します。
+        /// </summary>
+        /// <param name="value">値</param>
+        protected TypeSafeEnum(int value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// タイプセーフEnumの値を返します。
+        /// </summary>
+        public int Value { get; }
+
+        /// <summary>
+        /// 指定した値に該当するアイテムを取得します。
+        /// </summary>
+        /// <param name="value">値</param>
+        /// <returns>指定した値に該当するアイテムを返します。</returns>
+        /// <exception cref="ArgumentException">指定した値に該当するアイテムが存在しない場合に例外が投げられます。</exception>
+        public static T GetItem(int value)
+        {
+            var item = GetItems().SingleOrDefault(t => t.Value == value);
+            if (item == null) throw new KeyNotFoundException();
+            return item;
+        }
+
+        /// <summary>
+        /// タイプセーフEnumのアイテムを取得します。
+        /// </summary>
+        /// <returns>
+        /// タイプセーフEnumのアイテムを返します。
+        /// </returns>
+        public static IEnumerable<T> GetItems()
+        {
+            var fields = typeof(T).GetFields(
+                BindingFlags.Static |
+                BindingFlags.Public |
+                BindingFlags.DeclaredOnly);
+            return fields
+                .Select(field => field.GetValue(null))
+                .OfType<T>()
+                .ToList();
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTest/Tests/Domain/Models/Shared/TypeSafeEnum_Test.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HoneyRyderTask.Domain.Models.Shared;
+using Xunit;
+
+#pragma warning disable SA1401
+
+namespace HoneyRyderTaskTest.Tests.Domain.Models.Shared
+{
+    /// <summary>
+    /// TypeSafeEnum - test
+    /// </summary>
+    public class TypeSafeEnum_Test
+    {
+        /// <summary>
+        /// TypeSafeEnum（抽象クラス）のテストである為、テスト用に作成した実装クラスを使用する。
+        /// </summary>
+        private record TestTypeSafeEnum : TypeSafeEnum<TestTypeSafeEnum>
+        {
+            public static TestTypeSafeEnum Item1 = new TestTypeSafeEnum(value: 1);
+            public static TestTypeSafeEnum Item2 = new TestTypeSafeEnum(value: 2);
+            public static TestTypeSafeEnum Item3 = new TestTypeSafeEnum(value: 3);
+
+            private TestTypeSafeEnum(int value)
+                : base(value)
+            {
+            }
+        }
+
+        [Fact(DisplayName = "[GetItem(value)] 指定した値に該当するアイテムが取得できる。")]
+        public void GetItem_Test1()
+        {
+            // arrange
+            var value = 1;
+
+            // act
+            var actual = TestTypeSafeEnum.GetItem(value);     // 抽象クラスのテストである為、テスト用に作成した実装クラスを使用する。
+
+            // assert
+            Assert.Equal(actual, TestTypeSafeEnum.Item1);
+        }
+
+        [Fact(DisplayName = "[GetItem(value)] 指定した値に該当するアイテムが存在しない場合は例外が投げられる。")]
+        public void GetItem_Test2()
+        {
+            // arrange
+            var value = 4;
+
+            // act
+            var act = () => TestTypeSafeEnum.GetItem(value);  // 抽象クラスのテストである為、テスト用に作成した実装クラスを使用する。
+
+            // assert
+            Assert.Throws<KeyNotFoundException>(act);
+        }
+
+        [Fact(DisplayName = "[GetItems()] 定義してあるアイテムを全て取得できる。")]
+        public void GetItems_Test1()
+        {
+            // act
+            var items = TestTypeSafeEnum.GetItems();          // 抽象クラスのテストである為、テスト用に作成した実装クラスを使用する。
+
+            // assert
+            Assert.Equal(3, items.Count());
+            Assert.Equal(TestTypeSafeEnum.Item1, items.ElementAt(0));
+            Assert.Equal(TestTypeSafeEnum.Item2, items.ElementAt(1));
+            Assert.Equal(TestTypeSafeEnum.Item3, items.ElementAt(2));
+        }
+    }
+}
