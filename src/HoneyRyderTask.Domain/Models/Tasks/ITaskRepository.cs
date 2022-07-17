deb974270a53fd07557ffb956cf3ae60be6e13a8
ak-solo <ak.solo.2022@gmail.com>
2022-07-17 16:13:29 +0900

    タスクリポジトリにFindメソッド追加

--- src/HoneyRyderTask.Domain/Models/Tasks/ITaskRepository.cs
+++ src/HoneyRyderTask.Domain/Models/Tasks/ITaskRepository.cs
@@ -8,6 +8,13 @@ namespace HoneyRyderTask.Domain.Models.Tasks
     public interface ITaskRepository
     {
         /// <summary>
+        /// リポジトリからタスクIDに該当するタスクを取得します。
+        /// </summary>
+        /// <param name="taskId">タスクID</param>
+        /// <returns>タスクIDに該当するタスクを返します。</returns>
+        Task? Find(TaskId taskId);
+
+        /// <summary>
         /// タスクをリポジトリに追加します。
         /// </summary>
         /// <param name="task">タスク</param>
--- src/HoneyRyderTask.Infrastructure/PostgreSQL/Repositories/Tasks/TaskRepository.cs
+++ src/HoneyRyderTask.Infrastructure/PostgreSQL/Repositories/Tasks/TaskRepository.cs
@@ -1,5 +1,6 @@
 ﻿using HoneyRyderTask.Domain.Models.Tasks;
 using HoneyRyderTask.Infrastructure.PostgreSQL.DataModels.Tasks;
+using Task = HoneyRyderTask.Domain.Models.Tasks.Task;
 using TaskStatus = HoneyRyderTask.Domain.Models.Tasks.TaskStatus;
 
 namespace HoneyRyderTask.Infrastructure.PostgreSQL.Repositories.Tasks
@@ -21,17 +22,41 @@ namespace HoneyRyderTask.Infrastructure.PostgreSQL.Repositories.Tasks
         }
 
         /// <summary>
+        /// リポジトリからタスクIDに該当するタスクを取得します。
+        /// </summary>
+        /// <param name="taskId">タスクID</param>
+        /// <returns>タスクIDに該当するタスクを返します。</returns>
+        public Task? Find(TaskId taskId)
+        {
+            var data = this.context.Tasks.Find(taskId.Value);
+            if (data == null) return null;
+            return ToDomainModel(data);
+        }
+
+        /// <summary>
         /// タスクをリポジトリに追加します。
         /// </summary>
         /// <param name="task">タスク</param>
-        public void Add(Domain.Models.Tasks.Task task)
+        public void Add(Task task)
         {
             var data = ToDataModel(task);
             this.context.Add(data);
             this.context.SaveChanges();
         }
 
-        private static TaskDataModel ToDataModel(Domain.Models.Tasks.Task task)
+        private static Task ToDomainModel(TaskDataModel data)
+        {
+            return Task.Reconstruct(
+                id: TaskId.Create(data.TaskId),
+                title: TaskTitle.Create(data.Title),
+                description: TaskDescription.Create(data.Description),
+                status: TaskStatus.GetItem(data.Status),
+                dueDate: TaskDueDate.CreateNullable(data.DueDate),
+                creationDate: TaskCreationDate.Create(data.CreationDate),
+                completionDate: TaskCompletionDate.CreateNullable(data.CompletionDate));
+        }
+
+        private static TaskDataModel ToDataModel(Task task)
         {
             return new TaskDataModel()
             {
